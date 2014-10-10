35da19d7b1d6e957153d0eb59d7dad537604039d
Wassili Hense <hense.wassili@googlemail.com>
2014-10-10 16:59:50 +0200

    Engine ADD Server

---
+++ Engine/Server/ApiV04.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+using X13.PLC;
+
+namespace X13.Server {
+  internal class ApiV04 : WebSocketBehavior {
+    private static Topic _verbose;
+    private static Timer _pingTimer;
+    private static WebSocketSessionManager _wsMan;
+    private static string _hostName;
+
+    static ApiV04() {
+      _verbose=Topic.root.Get("/etc/Server/verbose");
+      _pingTimer=new Timer(PingF, null, 270000, 300000);
+      _hostName=Environment.MachineName;
+    }
+    private static void PingF(object o) {
+      if(_wsMan!=null) {
+        _wsMan.Broadping();
+      }
+    }
+
+    private Session _ses;
+
+    protected override void OnOpen() {
+      if(_wsMan==null) {
+        _wsMan=Sessions;
+      }
+      string sid=null;
+      if(Context.CookieCollection["sessionId"]!=null) {
+        sid=Context.CookieCollection["sessionId"].Value;
+      }
+      System.Net.IPEndPoint remoteEndPoint = Context.UserEndPoint;
+      {
+        System.Net.IPAddress remIP;
+        if(Context.Headers.Contains("X-Real-IP") && System.Net.IPAddress.TryParse(Context.Headers["X-Real-IP"], out remIP)) {
+          remoteEndPoint=new System.Net.IPEndPoint(remIP, remoteEndPoint.Port);
+        }
+      }
+      _ses=Session.Get(sid, remoteEndPoint, false);
+      if(_ses!=null) {
+        Send((new MessageV04(MessageV04.Cmd.Ack, 0, _ses.id)).ToString());
+      } else {
+        Send((new MessageV04(MessageV04.Cmd.Info, 0, _hostName)).ToString());
+      }
+      Send(string.Concat("I\t", _ses.id, "\t", string.IsNullOrEmpty(_ses.userName)?"null":"true") );
+      if(_verbose.As<bool>()) {
+        X13.lib.Log.Debug("{0} connect webSocket", _ses.owner.name);
+      }
+    }
+    protected override void OnMessage(MessageEventArgs e) {
+    }
+    protected override void OnError(ErrorEventArgs e) {
+    }
+    protected override void OnClose(CloseEventArgs e) {
+    }
+  }
+}
---
+++ Engine/Server/Server.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+using X13.lib;
+using X13.PLC;
+
+namespace X13.Server {
+  internal class Server {
+    private HttpServer _sv;
+    private Topic verbose;
+
+    public Server() {
+
+    }
+    public void Start() {
+      verbose=Topic.root.Get("/etc/Server/verbose");
+      _sv = new HttpServer(8080);
+      _sv.Log.Output=WsLog;
+#if DEBUG
+      _sv.Log.Level=WebSocketSharp.LogLevel.Trace;
+#endif
+      _sv.AddWebSocketService<ApiV04>("/api/v04");
+      _sv.Start();
+      if(_sv.IsListening) {
+        Log.Info("Server started on {0}:{1} ", Environment.MachineName, _sv.Port.ToString());
+      } else {
+        Log.Error("Server start failed");
+      }
+    }
+    public void Stop() {
+      if(_sv!=null) {
+        _sv.Stop();
+      }
+      _sv=null;
+    }
+    private void WsLog(LogData d, string f) {
+      if(verbose.As<bool>()) {
+        Log.Debug("WS({0}) - {1}", d.Level, d.Message);
+      }
+    }
+  }
+}
