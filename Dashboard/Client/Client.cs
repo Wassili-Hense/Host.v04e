690aebd0a4562b9e71ca57aa0ce20f375a903d41
Wassili Hense <hense.wassili@googlemail.com>
2014-10-12 21:09:18 +0200

    Dashboard create

---
+++ Dashboard/Client/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+
+namespace X13.Client {
+  internal class Client {
+    private System.ComponentModel.BackgroundWorker _bgw;
+    private ConcurrentQueue<MessageV04> _toSend;
+    private ConcurrentQueue<MessageV04> _recived;
+
+    public Client() {
+    }
+    public void Start() {
+      _bgw=new System.ComponentModel.BackgroundWorker();
+      _bgw.WorkerReportsProgress=true;
+      _bgw.ProgressChanged+=ClientCB;
+    }
+    public void Stop() {
+    }
+
+    private void ClientCB(object sender, System.ComponentModel.ProgressChangedEventArgs e) {
+      throw new NotImplementedException();
+    }
+
+  }
+}
---
+++ Dashboard/Dashboard.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace X13 {
+  public partial class Dashboard: Window {
+    private Client.Client _client;
+    public Dashboard() {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e) {
+      _client=new Client.Client();
+      _client.Start();
+    }
+
+    private void Window_Closing(object sender, CancelEventArgs e) {
+      _client.Stop();
+    }
+  }
+}
